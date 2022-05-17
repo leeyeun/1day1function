c9c588556ac027f8df90b67f40cffe22a6f5237f
leeyeun <dpdms0109@naver.com>
2022-05-17 23:56:51 +0900

    2day-05/17

---
+++ 2day.js
@@ -0,0 +1,13 @@
+//a부터 b까지 더하는 함수
+//5월 17일
+
+function sumAll(a, b) {
+    let output = 0;
+    for (let i = a; i <= b; i++){
+        output += i
+    }
+    return output;
+}
+
+console.log(`1부터 100까지 합은? ${sumAll(1, 100)}`);
+console.log(`1부터 500까지 합은? ${sumAll(1, 500)}`);
